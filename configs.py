61289b614aca70c64e418ceb726eb6fbd99134cc
wongkiyan <90238500+wongkiyan@users.noreply.github.com>
2023-10-03 23:51:25 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,14 +19,10 @@ APP_ICON_PATH = os.path.abspath("Takodachi_logo.png")
 
 # Path setting
 ARCHIVE_YOUTUBE_STREAM_BAT_PATCH             = "C:/Users/User/Documents/VtuberDownloadTools/yt-dlp/yt-dlp_ArchiveYoutubeStream.bat"
-
 ARCHIVE_VIDEO_BAT_PATCH                      = "C:/Users/User/Documents/VtuberDownloadTools/yt-dlp/yt-dlp_ArchiveYoutubeVideoWithAria2.bat"
-ARCHIVE_VIDEO_NO_PROGRESS_BAT_PATCH          = "C:/Users/User/Documents/VtuberDownloadTools/yt-dlp/yt-dlp_ArchiveYoutubeVideoWithAria2_NoProgress.bat"
 
 ARCHIVE_AND_PLAY_TWITCH_STREAM_BAT_PATCH     = "C:/Users/User/Documents/VtuberDownloadTools/streamlink/streamlink_ArchiveWithPlay.bat"
-
 ARCHIVE_TWITCH_STREAM_BAT_PATCH              = "C:/Users/User/Documents/VtuberDownloadTools/streamlink/streamlink_ArchiveWithoutPlay.bat"
-ARCHIVE_TWITCH_STREAM_NO_PROGRESS_BAT_PATCH  = "C:/Users/User/Documents/VtuberDownloadTools/streamlink/streamlink_ArchiveWithoutPlay_NoProgress.bat"
 
 STATUS_PATCH = "C:/Users/User/Documents/Discord/TakodachiBot_Status.bat"
 
